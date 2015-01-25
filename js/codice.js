9f4971c1de21be75c504fcbe22546130e0c15461
Nikipr <nicoletta.preziosi@gmail.com>
2015-01-25 19:19:22 +0100

    secondo

--- js/codice.js
+++ js/codice.js
@@ -2,7 +2,56 @@
  * Created by Nicoletta on 02/10/2014.
  */
 $(document).ready(function(){
-    $(".titoli").hover(function(){
-        $("body").toggleClass("immagine");
+    $(".uno").hover(function(){
+        $("body").css({"background-image": "url(img/a6.jpg)"});
+    });
+
+    $(".due").hover(function(){
+        $("body").css({"background-image": "url(img/bubbaloo.jpg)"});
+    });
+
+    $(".tre").hover(function(){
+        $("body").css({"background-image": "url(img/caus.jpg)"});
+    });
+
+    $(".quattro").hover(function(){
+        $("body").css({"background-image": "url(img/danone.jpg)"});
+    });
+
+    $(".cinque").hover(function(){
+        $("body").css({"background-image": "url(img/dettol.jpg)"});
+    });
+
+    $(".sei").hover(function(){
+        $("body").css({"background-image": "url(img/globo.jpg)"});
+    });
+
+    $(".sette").hover(function(){
+        $("body").css({"background-image": "url(img/nutriclub.jpg)"});
+    });
+
+    $(".otto").hover(function(){
+        $("body").css({"background-image": "url(img/paula.jpg)"});
+    });
+
+    $(".nove").hover(function(){
+        $("body").css({"background-image": "url(img/petmap.jpg)"});
+    });
+
+    $(".dieci").hover(function(){
+        $("body").css({"background-image": "url(img/qix.jpg)"});
+    });
+
+    $(".undici").hover(function(){
+        $("body").css({"background-image": "url(img/quadra.jpg)"});
+    });
+
+    $(".dodici").hover(function(){
+        $("body").css({"background-image": "url(img/uosh.jpg)"});
+    });
+    var lista = $( "header, footer" )
+
+    lista.hover(function(){
+        $("body").css({"background": "#1C1C1C"});
     });
 });
